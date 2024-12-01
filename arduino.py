25e2d8c80e007622785b7d0d8bdac1b9aaf80a61
sebaweiss <sebaweiss09@gmail.com>
2024-12-01 12:49:58 -0300

    Delete arduino.py

--- arduino.py
+++
@@ -1,7 +0,0 @@
-import serial
-import time
-
-def conexion_arduino():
-    arduino = serial.Serial(port='COM3', baudrate=9600, timeout=1)  # Cambia COM3 por tu puerto
-    time.sleep(2)  # Espera 2 segundos para que Arduino se estabilice
-    return arduino
